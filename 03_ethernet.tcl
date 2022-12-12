3699c4d76715b7d0b5747e4061ca66d5d98a3a3a
Hugh <70481919+ObnoxiousHugh@users.noreply.github.com>
2022-12-13 01:24:00 +0530

    Update 03_ethernet.tcl

--- 03_ethernet.tcl
+++ 03_ethernet.tcl
@@ -1,7 +1,7 @@
 set ns [new Simulator]
-set nf [open lab3.nam w]
+set nf [open PB3.nam w]
 $ns namtrace-all $nf
-set nd [open lab3.tr w]
+set nd [open PB3.tr w]
 $ns trace-all $nd
 $ns color 1 Blue
 $ns color 2 Red
@@ -10,7 +10,7 @@ global ns nf nd
 $ns flush-trace
 close $nf
 close $nd
-exec nam lab3.nam &
+exec nam PB3.nam &
 exit 0
 }
 set n0 [$ns node]
@@ -26,15 +26,15 @@ $n7 shape box
 $n7 color Blue
 $n8 shape hexagon
 $n8 color Red
-$ns duplex-link $n1 $n0 2Mb 10ms DropTail
-$ns duplex-link $n2 $n0 2Mb 10ms DropTail
-$ns duplex-link $n0 $n3 1Mb 20ms DropTail
+$ns duplex-link $n1 $n0 2Mb 1ms DropTail
+$ns duplex-link $n2 $n0 2Mb 1ms DropTail
+$ns duplex-link $n0 $n3 1Mb 2ms DropTail
 $ns make-lan "$n3 $n4 $n5 $n6 $n7 $n8" 512Kb 40ms LL Queue/DropTail Mac/802_3
 $ns duplex-link-op $n1 $n0 orient right-down
 $ns duplex-link-op $n2 $n0 orient right-up
 $ns duplex-link-op $n0 $n3 orient right
 $ns queue-limit $n0 $n3 20
-set tcp1 [new Agent/TCP/Vegas]
+set tcp1 [new Agent/TCP]
 $ns attach-agent $n1 $tcp1
 set sink1 [new Agent/TCPSink]
 $ns attach-agent $n7 $sink1
@@ -47,8 +47,21 @@ set tfile [open cwnd.tr w]
 $tcp1 attach $tfile
 $tcp1 trace cwnd_
 $ns at 0.5 "$ftp1 start"
-$ns at 1.0 "$ftp2 start"
-$ns at 5.0 "$ftp2 stop"
 $ns at 5.0 "$ftp1 stop"
 $ns at 5.5 "finish"
 $ns run
+
+
+BEGIN {
+}
+{
+if($6= ="cwnd_")
+printf("%f\t%f\t\n",$1,$7);
+}
+END {
+}
+
+
+ns PB3.tcl
+awk –f PB3.awk cwnd.tr > tcp1
+xgraph -x "time" -y "convalue" tcp1
